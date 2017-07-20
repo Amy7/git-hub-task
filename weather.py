4468f68d447d6a6adf60e541888a5888af7a16b5
Amy7 <Amyquan@mailinator.com>
2017-07-19 20:19:45 -0400

    Add files via upload

--- weather.py
+++ weather.py
@@ -1,7 +1,9 @@
-a = 70
-if a>= 90:
-    print ("it is hot outside")
-elif a<90 or a>60:
-    print ("it is warm outside")
-else:
-    print ("it is chilly outside")
+def weather():
+    a = 70
+    if a>= 90:
+        print ("it is hot outside")
+    elif a<90 or a>60:
+        print ("it is warm outside")
+    else:
+        print ("it is chilly outside")
+weather()
