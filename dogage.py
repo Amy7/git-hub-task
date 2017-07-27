578b9d3d06b795672c1efeb2aea2358fe40a71ac
Amy7 <Amyquan@mailinator.com>
2017-07-27 19:06:11 -0400

    Add files via upload

--- dogage.py
+++ dogage.py
@@ -1,8 +1,12 @@
+
 month = int(input("input months:"))
 year = int (input("input years:"))
-if (month<7):
-    old = 2017-year
-    print("he is born in",old)
-else:
-    old = 2017-1-year
-    print("he is born in", old)
+humenmonth = 7*month
+humenyear = 7*year + (humenmonth//12)
+print("the dog years is about",humenyear)
+
+
+
+
+
+
